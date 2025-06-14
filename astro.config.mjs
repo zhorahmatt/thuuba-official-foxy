d3807010ace40300a6e1c18ad19cefac7e5826dc
zhorahmatt <info.rahmathidayat@gmail.com>
2025-06-14 09:34:35 +0800

    feat: work featured page, modify some text desc

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://foxi.netlify.app/",
+  site: "https://thuuba.com",
   integrations: [
     tailwind(),
     icon(),
